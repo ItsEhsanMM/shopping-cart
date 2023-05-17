3e901343e0a0c16ff5b61aa6effc47689c432e8a
ItsEhsanMM <itsehsanmm@gmail.com>
2023-05-17 21:02:46 +0330

    navbar styled

--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -7,15 +7,18 @@ import shopIcon from "../../assets/icons/shop.svg";
 // Context
 import { CartContext } from "../../context/CartContextProvider";
 
+// Style
+import styles from './Navbar.module.scss'
+
 const Navbar = () => {
    const { state } = useContext(CartContext);
 
    return (
-      <div>
+      <div className={styles.container}>
          <Link to="/products">Products</Link>
 
-         <div>
-            <Link to="/cart">
+         <div className={styles.cartContainer}>
+            <Link to="/cart" className={styles.cart}>
                <img src={shopIcon} alt="cart" style={{ width: "30px" }} />
             </Link>
             <span>{state.itemCounter}</span>
