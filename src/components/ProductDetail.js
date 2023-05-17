803f13057ae336abcfd6375629568cf62de78cd2
ItsEhsanMM <itsehsanmm@gmail.com>
2023-05-17 17:21:22 +0330

    error in reloading product Detail page fixed with conditional rendering and get items in the same page

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -5,30 +5,33 @@ import { Link, useParams } from "react-router-dom";
 import { productsContext } from "../context/ProductsContextProvider";
 
 const ProductDetail = () => {
-  const data = useContext(productsContext);
-  const param = useParams().id;
-  const product = data[param - 1];
+   const data = useContext(productsContext);
+   const param = useParams().id;
+   const newData = data.filter((item) => item.id === param - 0);
 
-  const { image, title, description, category, price } = product;
+   if (newData.length === 0) {
+      return <h1>Loading...</h1>;
+   } else {
+      const { image, title, description, category, price } = newData[0];
 
-  return (
-    <div>
-      <img src={image} alt="product" style={{width : '200px'}} />
-      <div>
-        <h3>{title}</h3>
-        <p>{description}</p>
-        <p>
-          <span>category :</span> {category}
-        </p>
+      return (
+         <div>
+            <img src={image} alt="product" style={{ width: "200px" }} />
 
-        <div>
-          <p>{price} $</p>
-
-          <Link to="/">Back to product</Link>
-        </div>
-      </div>
-    </div>
-  );
+            <div>
+               <h3>{title}</h3>
+               <p>{description}</p>
+               <p>
+                  <span>category :</span> {category}
+               </p>
+               <div>
+                  <p>{price} $</p>
+                  <Link to="/">Back to product</Link>
+               </div>
+            </div>
+         </div>
+      );
+   }
 };
 
 export default ProductDetail;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
+// global Styles
+import './index.scss'
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
