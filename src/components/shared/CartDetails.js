e16f4559defa019f17c98e6a6c5852c9865529e8
ItsEhsanMM <itsehsanmm@gmail.com>
2023-05-18 03:31:57 +0330

    cart page responsived for mobile

--- src/components/shared/CartDetails.js
+++ src/components/shared/CartDetails.js
@@ -9,40 +9,39 @@ import trashIcon from "../../assets/icons/trash.svg";
 // Context
 import { CartContext } from "../../context/CartContextProvider";
 
+// Styles
+import Styles from "./CartDetail.module.scss";
+
 const CartDetails = ({ data }) => {
    const { image, title, price, quantity } = data;
 
    const { dispatch } = useContext(CartContext);
 
    return (
-      <div>
+      <div className={Styles.container}>
          <img src={image} alt="product" style={{ width: "100px" }} />
          <div>
             <h2>{shorten(title)}</h2>
             <h3>{price} $</h3>
          </div>
-         <div>
-            <span>{quantity}</span>
-         </div>
-         <div>
-            {quantity === 1 ? (
-               <button
-                  onClick={() => dispatch({ type: "REMOVE", payload: data })}
-               >
-                  <img src={trashIcon} alt="remove"  style={{width: '10px'}}/>
+         <div className={Styles.spanaBtn}>
+            <div>
+               <span>{quantity}</span>
+            </div>
+            <div className={Styles.buttons}>
+               {quantity === 1 ? (
+                  <button onClick={() => dispatch({ type: "REMOVE", payload: data })}>
+                     <img src={trashIcon} alt="remove" style={{ width: "10px" }} />
+                  </button>
+               ) : (
+                  <button className={Styles.minus} onClick={() => dispatch({ type: "DECREASE", payload: data })}>
+                     -
+                  </button>
+               )}
+               <button className={Styles.plus} onClick={() => dispatch({ type: "INCREASE", payload: data })}>
+                  +
                </button>
-            ) : (
-               <button
-                  onClick={() => dispatch({ type: "DECREASE", payload: data })}
-               >
-                  -
-               </button>
-            )}
-            <button
-               onClick={() => dispatch({ type: "INCREASE", payload: data })}
-            >
-               +
-            </button>
+            </div>
          </div>
       </div>
    );
